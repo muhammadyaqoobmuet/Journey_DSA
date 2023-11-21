95a35a9a9e1d5e11eff5ba03b12ccfc9679a87f2
Muhammad Yaqoob <121377745+muhammadyaqoobmuet@users.noreply.github.com>
2023-11-21 19:36:18 +0500

    Time: 1 ms (97.45%), Space: 40.2 MB (9.31%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-         return (int)Math.pow( x, 0.5);
+
+   long START = 0, END = x;
+    while (START <= END) {
+        long MID = START + (END - START) / 2;
+
+      if (MID * MID == x) {
+        return (int) MID;
+      } else if (MID * MID < x) {
+        START = MID + 1;
+      } else {
+        
+        END = MID - 1;
+      }
     }
+    return (int) START - 1;
+}
 }
